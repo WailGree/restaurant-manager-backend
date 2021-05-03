91262bf7b4bde91549265c9de39ae9eb01e0a707
Mate Szathmari <mateszathmari@gmail.com>
2021-05-03 13:31:29 +0200

    Add User model

---
+++ EFDataAccess/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFDataAccess.Models
+{
+    public class User
+    {
+        [Key] [MaxLength(100)] public string UserName { get; set; }
+        [Required] [MaxLength(200)] public string Email { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(string userName, string email)
+        {
+            UserName = userName;
+            Email = email;
+        }
+    }
+}
