de457d5ac7c454ea88d36fbae7e777110cb4d642
Mate Szathmari <mateszathmari@gmail.com>
2021-05-04 13:19:53 +0200

    Add api controller

---
+++ RestaurantManager.Web/Controllers/ApiController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFDataAccess.DataAccess;
+using EFDataAccess.Models;
+using RecipesAPI.Models;
+
+namespace RestaurantManager.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        private readonly RestaurantContext _context;
+        private readonly SQLMenuRepository _menu;
+        private readonly SQLUserRepository _users;
+
+        public ApiController(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet()]
+        public string GetFavoriteRecipes()
+        {
+            return "data";
+        }
+
+
+
+    }
+}
