106ba1d2430e8b15073275ab314c5ff9a80fa688
Mate Szathmari <mateszathmari@gmail.com>
2021-05-04 13:40:14 +0200

    Add user migration

--- EFDataAccess/Migrations/20210504113812_user.Designer.cs
+++ EFDataAccess/Migrations/20210504113812_user.Designer.cs
@@ -3,14 +3,16 @@ using EFDataAccess.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EFDataAccess.Migrations
 {
     [DbContext(typeof(RestaurantContext))]
-    partial class RestaurantContextModelSnapshot : ModelSnapshot
+    [Migration("20210504113812_user")]
+    partial class user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
