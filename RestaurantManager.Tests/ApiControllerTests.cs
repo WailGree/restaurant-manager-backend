25b760c44a69cdc2f821c212f23d92c66c6fc1d8
Mate Szathmari <mateszathmari@gmail.com>
2021-05-05 12:38:25 +0200

    Add registration test

--- RestaurantManager.Tests/ApiControllerTests.cs
+++ RestaurantManager.Tests/ApiControllerTests.cs
@@ -23,6 +23,26 @@ namespace RestaurantManager.Tests
             _client = fixture.CreateClient();
         }
 
-        
+        [Test]
+        public async Task Test1_Registration_ValidCredential_ShouldReturnOk()
+        {
+            // Arrange
+            string url = "api/registration";
+            RegistrationCredentials registrationCred =
+                new RegistrationCredentials("username", "password", "email@email.com");
+            string output = JsonConvert.SerializeObject(registrationCred);
+            var req = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(output,
+                    Encoding.UTF8, "application/json")
+            };
+
+            // Act
+            var response = await _client.SendAsync(req);
+
+            // Assert
+            Assert.IsTrue(response.StatusCode == HttpStatusCode.OK);
+        }
+
     }
 }
