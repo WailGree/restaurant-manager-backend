aa7af34d846a99ad648ea5f6c3b220a1f6960ea9
Mate Szathmari <mateszathmari@gmail.com>
2021-05-05 12:37:55 +0200

    Setup test environment

---
+++ RestaurantManager.Tests/ApiControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using EFDataAccess.DataAccess;
+using EFDataAccess.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+using RestaurantManager.Web;
+using Xunit;
+
+namespace RestaurantManager.Tests
+{
+    public class ApiControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private HttpClient _client { get; }
+
+        public ApiControllerTests()
+        {
+            var fixture = new WebApplicationFactory<Startup>();
+            _client = fixture.CreateClient();
+        }
+
+        
+    }
+}
